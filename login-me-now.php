66aa9ca81a42489c51740f1a7020be5e53407343
Mehedi Hasan  ⚡️ <mehedi.2800@gmail.com>
2025-02-27 20:26:48 +0600

    tested up to 6.7.2

--- login-me-now.php
+++ login-me-now.php
@@ -5,7 +5,7 @@
  * Author: Pluginly
  * Author URI: https://loginmenow.com/
  * Version: 1.7.3
- * Tested up to: 6.6
+ * Tested up to: 6.7.2
  * Requires PHP: 7.4
  * License: GPLv2 or later
  * Text Domain: login-me-now
